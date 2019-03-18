bdd32c6c37606763407deb77d329231663d32c2f
Rav gowda <ravish.nfs@gmail.com>
2019-03-19 02:09:42 +0530

    put together

---
+++ androjupy.sh
@@ -0,0 +1,12 @@
+################################################
+pkg update
+pkg install proot
+termux-chroot
+apt install git clang
+apt install pkg-config
+apt install python python3-dev 
+apt install libclang libclang-dev
+apt install libzmq libzmq-dev
+apt install ipython 
+pip install jupyter 
+###############################################
